d8a188483cced53f6fa2ead0dcd42a2fd5275b8d
Brandon Weaver <keystonelemur@gmail.com>
2015-10-19 12:05:59 -0700

    basic vim examples

--- vim_examples.js
+++ vim_examples.js
@@ -2,4 +2,16 @@
 // * https://robots.thoughtbot.com/vim-macros-and-you
 // * http://blog.sanctum.geek.nz/advanced-vim-macros/
 
+// To record a macro, press q followed by a letter for storage
+// We'll use 'qq' to store it in the 'q' buffer
+var a = 1;
 
+// 'd' to cut it
+// 'i' to insert 'function () {\n\n}'
+// move inside there
+// paste the code with 'p'
+// select all text inside the brackets with 'vi{'
+// indent it with >>
+//
+// Try it again here:
+var b = 2;
